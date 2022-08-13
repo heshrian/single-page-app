5cf417b43a1332e7cef8284e2d7684c77c9bd76d
heshrian <kamburovviktor@gmail.com>
2022-08-13 13:51:10 +0100

    props

--- my-single-page-app/src/components/ExpenseItem.js
+++ my-single-page-app/src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div>Date</div>
-      <div className="expense-item__description">
-        <h2>Title</h2>
+      <div>
+        {/* <div>{props.date.toLocaleString("en-US", { day: "numeric",month: "long",year: "numeric" })}</div> */}
+
+        <div>{props.date.toLocaleString("en-US", { day: "numeric" })}</div>
+        <div>{props.date.toLocaleString("en-US", { month: "long" })}</div>
+        <div>{props.date.toLocaleString("en-US", { year: "numeric" })}</div>
       </div>
-      <div className="expense-item__price">Amount</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item__price">£{props.amount}</div>
     </div>
   );
 };

7dd0b1a1990f8a9597ae4f31219a8b567c70623f
heshrian <kamburovviktor@gmail.com>
2022-08-13 12:53:17 +0100

    mintlify doc testing

--- my-single-page-app/src/App.js
+++ my-single-page-app/src/App.js
@@ -1,8 +1,13 @@
 
+/**
+ * The function App() returns a div element with two children: an h2 element and a p element
+ * @returns The function App is returning a div with a h2 and a p tag.
+ */
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p> this is also visible</p>
     </div>
   );
 }

873bbeb583c78b558f7abf7284def768831441ca
heshrian <kamburovviktor@gmail.com>
2022-08-13 13:06:37 +0100

    class is className

--- my-single-page-app/src/App.js
+++ my-single-page-app/src/App.js
@@ -1,13 +1,16 @@
-
 /**
  * The function App() returns a div element with two children: an h2 element and a p element
  * @returns The function App is returning a div with a h2 and a p tag.
  */
+
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p> this is also visible</p>
+      {/* <ExpenseItem></ExpenseItem> */}
+      <ExpenseItem />
     </div>
   );
 }
